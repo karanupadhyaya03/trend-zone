861e027752510eaf5694b90e851b1b6e0cc482f1
Karan Upadhyaya <karanupadayaya2003@gmail.com>
2023-07-22 12:20:42 +0530

    add style using bootstrap

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,26 +1,44 @@
-import './App.css';
+import { Col, Container, Nav, Navbar, Row } from 'react-bootstrap';
 import { sampleProducts } from './data';
 
 function App() {
   return (
-    <div>
-      <header>Trend zone</header>
+    <div className="d-flex flex-column vh-100">
+      <header>
+        <Navbar bg="primary" variant="dark" expand="lg">
+          <Container>
+            <Navbar.Brand>Trend zone</Navbar.Brand>
+          </Container>
+          <Nav>
+            <a href="/" className="nav-link">
+              Cart
+            </a>
+            <a href="/" className="nav-link">
+              Sign in
+            </a>
+          </Nav>
+        </Navbar>
+      </header>
       <main>
-        <ul>
-          {sampleProducts.map((product) => (
-            <li key={product.slug}>
-              <img
-                src={product.image}
-                alt={product.name}
-                className="product-image"
-              />
-              <h2>{product.name}</h2>
-              <p>${product.price}</p>
-            </li>
-          ))}
-        </ul>
+        <Container className="mt-3">
+          <Row>
+            {sampleProducts.map((product) => (
+              <Col key={product.slug} sm={6} md={4} lg={3}>
+                <img
+                  src={product.image}
+                  alt={product.name}
+                  className="product-image"
+                />
+                <h2>{product.name}</h2>
+                <p>${product.price}</p>
+              </Col>
+            ))}
+          </Row>
+        </Container>
       </main>
-      <footer>All rights reserved</footer>
+      <footer>
+        <div className="text-center">All rights reserved</div>
+      </footer>
     </div>
   );
 }
