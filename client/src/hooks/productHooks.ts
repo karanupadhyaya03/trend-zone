4f66cbf59e7151729b74251f58c00a1d448cdfcb
Karan Upadhyaya <karanupadayaya2003@gmail.com>
2023-07-27 23:18:59 +0530

    define mutation for sign in in frontend

--- client/src/hooks/productHooks.ts
+++ client/src/hooks/productHooks.ts
@@ -2,13 +2,11 @@ import { useQuery } from '@tanstack/react-query';
 import apiClient from '../apiClient';
 import { Product } from '../types/Product';
 
-export const useGetProductsQuery = () => {
-  return useQuery({
+export const useGetProductsQuery = () =>
+  useQuery({
     queryKey: ['products'],
     queryFn: async () => (await apiClient.get<Product[]>(`api/products`)).data,
   });
-};
-
 export const useGetProductDetailsBySlugQuery = (slug: string) => {
   return useQuery({
     queryKey: ['products', slug],
